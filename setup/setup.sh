e627c6db412ddcc2d994fecd06f095c0da2b7774
Christopher Klein <ckl@dreitier.com>
2025-08-25 14:45:59 +0200

    fix: quoting
    Signed-off-by: Christopher Klein <ckl@dreitier.com>

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,15 +3,15 @@ PWD=`pwd`
 MAKE_DOCKER_IMAGE_DIRECTORY=make-docker-image
 MAKEFILE_SHARED=Makefile.shared
 
-if [ ! -e $PWD/Makefile ]; then
+if [ ! -e "$PWD/Makefile" ]; then
 	echo "[info] No Makefile available in current directory, setting it up..."
 
 	# local development of make-docker-image
-	if [ -d $PWD/../$MAKE_DOCKER_IMAGE_DIRECTORY ]; then
+	if [ -d "$PWD/../$MAKE_DOCKER_IMAGE_DIRECTORY" ]; then
 		echo "[info] ../${MAKE_DOCKER_IMAGE_DIRECTORY} repository is available, using this one."
 		ln -s $PWD/../$MAKE_DOCKER_IMAGE_DIRECTORY/$MAKEFILE_SHARED $PWD/Makefile 
 	# submodule
-	elif [ ! -d $PWD/$MAKE_DOCKER_IMAGE_DIRECTORY/$MAKEFILE_SHARED ]; then
+	elif [ ! -d "$PWD/$MAKE_DOCKER_IMAGE_DIRECTORY/$MAKEFILE_SHARED" ]; then
 		echo "[info] Git submodule ${MAKE_DOCKER_IMAGE_DIRECTORY} has not been checked out yet. Cloning submodule..."
 		git submodule init
 
@@ -23,12 +23,12 @@ else
 	echo "[info] Makefile already linked :-)"
 fi
 
-if [ -e $PWD/$MAKE_DOCKER_IMAGE_DIRECTORY/$MAKEFILE_SHARED ]; then
+if [ -e "$PWD/$MAKE_DOCKER_IMAGE_DIRECTORY/$MAKEFILE_SHARED" ]; then
 	echo "[info] Updating make-docker-image ... "
 	git submodule update --remote
 fi
 
-if [ ! -e $PWD/Makefile ]; then
+if [ ! -e "$PWD/Makefile" ]; then
 	echo "[error] Could not link Makefile in this directory to any of the options!"
 	exit 1
 fi
