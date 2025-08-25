1d4cbf8dce6be386df530eafa94a10be62dfb1e1
Christopher Klein <ckl@dreitier.com>
2025-08-25 16:12:39 +0200

    fix: whitespaces in path name when linking
    Signed-off-by: Christopher Klein <ckl@dreitier.com>

--- setup/setup.sh
+++ setup/setup.sh
@@ -9,13 +9,13 @@ if [ ! -e "$PWD/Makefile" ]; then
 	# local development of make-docker-image
 	if [ -d "$PWD/../$MAKE_DOCKER_IMAGE_DIRECTORY" ]; then
 		echo "[info] ../${MAKE_DOCKER_IMAGE_DIRECTORY} repository is available, using this one."
-		ln -s $PWD/../$MAKE_DOCKER_IMAGE_DIRECTORY/$MAKEFILE_SHARED $PWD/Makefile 
+		ln -s "$PWD/../$MAKE_DOCKER_IMAGE_DIRECTORY/$MAKEFILE_SHARED" "$PWD/Makefile" 
 	# submodule
 	elif [ ! -d "$PWD/$MAKE_DOCKER_IMAGE_DIRECTORY/$MAKEFILE_SHARED" ]; then
 		echo "[info] Git submodule ${MAKE_DOCKER_IMAGE_DIRECTORY} has not been checked out yet. Cloning submodule..."
 		git submodule init
 
-		ln -s $PWD/$MAKE_DOCKER_IMAGE_DIRECTORY/$MAKEFILE_SHARED $PWD/Makefile 
+		ln -s "$PWD/$MAKE_DOCKER_IMAGE_DIRECTORY/$MAKEFILE_SHARED" "$PWD/Makefile"
 	else
 		echo "[warn] I have *no* idea how to set up the Makefile. Either use ../${MAKE_DOCKER_IMAGE_DIRECTORY} for local development or use the 'git submodule' method"
 	fi
